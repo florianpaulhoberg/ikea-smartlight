1465dd6202f79399a171811871321a20dcb501f1
Florian Paul Hoberg <florianpaulhoberg@users.noreply.github.com>
2018-03-10 08:02:36 +0100

    added support of dtls token

--- tradfri-lights.py
+++ tradfri-lights.py
@@ -4,10 +4,11 @@
 # purpose     : getting status from the Ikea tradfri smart lights
 #
 # author      : harald van der laan
-# date        : 2017/04/10
-# version     : v1.1.0
+# date        : 2018/03/10
+# version     : v1.1.1
 #
 # changelog   :
+# - v1.1.1      support for semi-static dTLS tokens                     (florian)
 # - v1.1.0      refactor for cleaner code                               (harald)
 # - v1.0.0      initial concept                                         (harald)
 
@@ -53,22 +54,23 @@ def main():
 
     hubip = conf.get('tradfri', 'hubip')
     securityid = conf.get('tradfri', 'securityid')
+    dtlstoken = conf.get('tradfri', 'dtlstoken')
 
     if args.action == 'power':
         if args.value == 'on' or args.value == 'off':
-            tradfriActions.tradfri_power_light(hubip, securityid, args.lightbulbid, args.value)
+            tradfriActions.tradfri_power_light(hubip, securityid, dtlstoken, args.lightbulbid, args.value)
         else:
             sys.stderr.write('[-] Tradfri: power state can only be on/off\n')
             sys.exit(1)
     elif args.action == 'brightness':
         if 1 <= int(args.value) <= 100:
-            tradfriActions.tradfri_dim_light(hubip, securityid, args.lightbulbid, args.value)
+            tradfriActions.tradfri_dim_light(hubip, securityid, dtlstoken, args.lightbulbid, args.value)
         else:
             sys.stderr.write('[-] Tradfri: dim value can only be between 1 and 100\n')
             sys.exit(1)
     elif args.action == 'color':
         if args.value == 'warm' or args.value == 'normal' or args.value == 'cold':
-            tradfriActions.tradfri_color_light(hubip, securityid, args.lightbulbid, args.value)
+            tradfriActions.tradfri_color_light(hubip, securityid, dtlstoken, args.lightbulbid, args.value)
         else:
             sys.stderr.write('[-] Tradfri: color value can only be warm/normal/cold\n')
             sys.exit(1)
