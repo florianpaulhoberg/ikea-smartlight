229d07e97b9006ec71a0ea2c50ac99403f422379
Florian Paul Hoberg <florianpaulhoberg@users.noreply.github.com>
2018-03-10 07:55:14 +0100

    added support for semi-static dTLS token

--- tradfri/tradfriStatus.py
+++ tradfri/tradfriStatus.py
@@ -29,10 +29,10 @@ import json
 global coap
 coap = '/usr/local/bin/coap-client'
 
-def tradfri_get_devices(hubip, securityid):
+def tradfri_get_devices(hubip, securityid), dtlstoken:
     """ function for getting all tradfri device ids """
     tradfriHub = 'coaps://{}:5684/15001' .format(hubip)
-    api = '{} -m get -u "Client_identity" -k "{}" "{}" | awk \'NR==4\'' .format(coap, securityid,
+    api = '{} -m get -u "{}" -k "{}" "{}" | awk \'NR==4\'' .format(coap, dtlstoken, securityid,
                                                                                 tradfriHub)
 
     if os.path.exists(coap):
@@ -43,10 +43,10 @@ def tradfri_get_devices(hubip, securityid):
 
     return json.loads(result.read().strip('\n'))
 
-def tradfri_get_lightbulb(hubip, securityid, deviceid):
+def tradfri_get_lightbulb(hubip, securityid, dtlstoken, deviceid):
     """ function for getting tradfri lightbulb information """
     tradfriHub = 'coaps://{}:5684/15001/{}' .format(hubip, deviceid)
-    api = '{} -m get -u "Client_identity" -k "{}" "{}" | awk \'NR==4\''.format(coap, securityid,
+    api = '{} -m get -u "{}" -k "{}" "{}" | awk \'NR==4\''.format(coap, dtlstoken, securityid,
                                                                                tradfriHub)
 
     if os.path.exists(coap):
@@ -57,10 +57,10 @@ def tradfri_get_lightbulb(hubip, securityid, deviceid):
 
     return json.loads(result.read().strip('\n'))
 
-def tradfri_get_groups(hubip, securityid):
+def tradfri_get_groups(hubip, securityid, dtlstoken):
     """ function for getting tradfri groups """
     tradfriHub = 'coaps://{}:5684/15004'.format(hubip)
-    api = '{} -m get -u "Client_identity" -k "{}" "{}" | awk \'NR==4\''.format(coap, securityid,
+    api = '{} -m get -u "{}" -k "{}" "{}" | awk \'NR==4\''.format(coap, dtlstoken, securityid,
                                                                                tradfriHub)
 
     if os.path.exists(coap):
@@ -71,10 +71,10 @@ def tradfri_get_groups(hubip, securityid):
 
     return json.loads(result.read().strip('\n'))
 
-def tradfri_get_group(hubip, securityid, groupid):
+def tradfri_get_group(hubip, securityid, dtlstoken, groupid):
     """ function for getting tradfri group information """
     tradfriHub = 'coaps://{}:5684/15004/{}'.format(hubip, groupid)
-    api = '{} -m get -u "Client_identity" -k "{}" "{}" | awk \'NR==4\''.format(coap, securityid,
+    api = '{} -m get -u "{}" -k "{}" "{}" | awk \'NR==4\''.format(coap, dtlstoken, securityid,
                                                                                tradfriHub)
 
     if os.path.exists(coap):
